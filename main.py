7fc86a6001f1d55f1df6f10b8a8fbeb8f865611f
ma-re-vo <124522247+ma-re-vo@users.noreply.github.com>
2026-04-27 01:00:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,26 @@
-from src.ingestion.reddit_stream import create_stream
-from src.graph.graph_builder import Graph, process_post
-from src.algorithms.fennel import FennelPartitioner
-from config import NUM_PARTITIONS, ALPHA, MAX_POSTS
-from src.visualization import draw_graph
-from src.algorithms.cluster_quality import compute_modularity, dummy_oslom_genperm
-from src.modular_decomp import modular_decomposition
-import networkx as nx
+from src.ingestion.pushshift_stream import PushshiftStream
+from src.graph.graph_builder import GraphBuilder
+from src.algorithms.fennel import FennelClustering
+from src.algorithms.cluster_quality import compute_modularity
+
 
 def main():
-    graph = Graph()
-    fennel = FennelPartitioner(graph, NUM_PARTITIONS, ALPHA)
+    stream = PushshiftStream("data/comments.jsonl", delay=0.0005)
+    graph_builder = GraphBuilder()
+    fennel = FennelClustering(k=5)
 
-    stream = create_stream()
+    step = 0
 
-    for i, post in enumerate(stream):
-        if i >= MAX_POSTS:
-            break
+    for data in stream.stream():
+        G = graph_builder.process(data)
+        clusters = fennel.run(G)
 
-        news_node = process_post(graph, post)
-        if news_node is None:
-            continue
+        step += 1
 
-        fennel.assign(news_node)
+        if step % 2000 == 0:
+            mod = compute_modularity(G, clusters)
+            print(f"Step {step} | Nodes: {len(G.nodes())} | Modularity: {mod:.4f}")
 
-    # --- кластеризация проверка OSLOM / GenPerm ---
-    partitions = dummy_oslom_genperm(graph, fennel.partitions)
-
-    # --- модульная декомпозиция ---
-    modular_clusters = modular_decomposition(graph)
-
-    # --- визуализация ---
-    draw_graph(graph, partitions=partitions)
-
-    # --- метрика качества ---
-    G_nx = nx.Graph()
-    for node, neighbors in graph.neighbors.items():
-        for n in neighbors:
-            G_nx.add_edge(node, n)
-    mod_score = compute_modularity(G_nx, partitions)
-    print("Modularity of Fennel clusters:", mod_score)
 
 if __name__ == "__main__":
     main()
