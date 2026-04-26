ff3ef5639f126935114e0bdd5c5d16860acd3159
ma-re-vo <124522247+ma-re-vo@users.noreply.github.com>
2026-04-27 01:07:08 +0200

    Update cluster_quality.py

--- cluster_quality.py
+++ cluster_quality.py
@@ -1,14 +1,8 @@
 import networkx as nx
-from networkx.algorithms.community import modularity
+from networkx.algorithms.community.quality import modularity
 
-def compute_modularity(graph, partitions):
-    communities = [list(p) for p in partitions]
-    return modularity(graph, communities)
 
-def dummy_oslom_genperm(graph, partitions):
-    """
-    Заглушка для OSLOM / GenPerm
-    Можно потом вызвать внешнюю реализацию через subprocess
-    """
-    # на текущий момент просто возвращаем partitions
-    return partitions
+def compute_modularity(G, clusters):
+    communities = list(clusters.values())
+    return modularity(G, communities)
+
